d7c6916b4409bfd842baf5cf0a9be27c61063bb0
kerrickwoyshner <kerrick.woyshner@gmail.com>
2018-07-10 17:14:16 -0400

    documentation added; movie titles listed

--- Library/Letter.js
+++ Library/Letter.js
@@ -1,20 +1,37 @@
-function Letter(letter) {
-    this.letter = letter;
-    this.guessed = false;
+// The Letter constructor is responsible for displaying either an underscore or the underlying character for each letter in the word
+
+function Letter(char) {
+    // If a character is not a number or a letter, make it visible right away
+    this.visible = !/[a-z1-9]/i.test(char);
+    // Save the underlying character
+    this.char = char;
 }
-// getLetter to string insteads
-Letter.prototype.getLetter = function () {
-    if (this.guessed) {
-        return this.letter;
-    } else {
-        return "_";
+
+// prototypes are optional
+
+// Returns either an underscore or the underlying character depending on `this.visible`
+//    because we call this function toString, when we call `this.letters.join` in
+//    Word.js, JavaScript automatically uses the value we return here
+Letter.prototype.toString = function () {
+    if (this.visible === true) {
+        return this.char;
     }
+    return "_";
 };
 
-Letter.prototype.guess = function (letter) {
-    if (this.letter === letter) {
-        this.guessed = true;
-    }
-}
+Letter.prototype.getSolution = function () {
+    return this.char;
+};
 
-module.exports = Letter;
+// Accepts a user's guess
+Letter.prototype.guess = function (charGuess) {
+    if (charGuess.toUpperCase() === this.char.toUpperCase()) {
+        this.visible = true;
+        return true;
+    }
+
+    // Otherwise return false
+    return false;
+};
+
+module.exports = Letter;  
