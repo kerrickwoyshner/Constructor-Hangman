e8f30874a7d5188fc56242db4a10c69af2a86166
kerrickwoyshner <kerrick.woyshner@gmail.com>
2018-07-10 17:15:31 -0400

    Initialized Game object & playing

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
-var inquirer = require("inquirer");
-const Letter = require("./Letter");
-const Word = require("./Word");
+var Game = require("./Library/Game");
 
-function chooseWord() {
-    var wordsToChoose = ["gorilla", "giraffe", "mouse", "lion", "bear"];
-    randomNumber = Math.round(Math.random() * wordsToChoose.length + 1);
-    var randomWord = new Word(wordsToChoose[randomNumber]);
-    console.log(randomWord);
-    console.log(randomWord.getWord());
-    randomWord.guess("l");
-};
+// Initialize a new Game object
+var game = new Game();
 
-function userLetterTyped(letter) {
-    inquirer.prompt([
-        {
-            name: "letter",
-            message: "Guess: ",
-        }
-    ]).then(function (response) {
-        var userLetter = new userLetterTyped(response.letter);
-    });
-};
+// Start playing
+game.play();
